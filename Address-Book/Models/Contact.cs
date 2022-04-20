1367bba4fa227e4d442d71ab46bf52d286de3826
Neerav Patel <niravpatel4848@gmail.com>
2022-04-19 21:15:28 -0600

    Add project files.

---
+++ Address-Book/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿namespace Address_Book.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Country { get; set; }
+    }
+}
